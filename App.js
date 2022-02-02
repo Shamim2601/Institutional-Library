84140c123a0a76df83e7c43ad1f3b0e93194dd7f
shamim2601 <shamim2601@gmail.com>
2022-02-02 22:58:54 +0600

    done

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
-const oracledb = require('oracledb');
+//const oracledb = require('oracledb');
 var http = require('http');
-var url  = require('url');
+var url = require('url');
+var fs = require('fs');
 
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "html" + q.pathname;
+  
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+  
+}).listen(8080);
+
+/*
 async function run() {
 
   let connection;
@@ -56,3 +74,4 @@ async function run() {
 }
 
 run();
+*/
