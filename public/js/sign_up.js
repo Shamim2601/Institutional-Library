b662d753c244132ab7de97bf5ae12fba7308d4f4
Asif Ihtemadul Haque <asifihtemadulhaque@gmail.com>
2022-02-09 13:48:05 +0600

    user_id added to applicant

--- public/js/sign_up.js
+++ public/js/sign_up.js
@@ -10,11 +10,12 @@ router.get('/',(req,res)=>{
 
 router.post("/",async function(req,res){
     let {name,designation,department,id,dob,address,email,phone,blood_group,residence,password} = req.body;
-    const query = `INSERT INTO APPLICANT (NAME,DESIGNATION,DEPT,ID,DATE_OF_BIRTH,ADDRESS,EMAIL,PHONE_NUMBER,BLOOD_GROUP,RESIDENCE,PASSWORD) VALUES
-     (:1, :2, :3, :4, :5, :6, :7, :8, :9,:10, :11)`
+    let user_id = Date.now().toString();
+    const query = `INSERT INTO APPLICANT (NAME,DESIGNATION,DEPT,ID,DATE_OF_BIRTH,ADDRESS,EMAIL,PHONE_NUMBER,BLOOD_GROUP,RESIDENCE,PASSWORD,USER_ID) VALUES
+     (:1, :2, :3, :4, :5, :6, :7, :8, :9,:10, :11, :12)`
      // const query = `INSERT INTO APPLICANT VALUES('esse','EsE','TEACHER','18343', '11-feb-98','DHAKA','A@A',12345678911,'B+','DHAKA','111a')`
     let params = [name,designation,department,id.toString(),dob,address,email,phone,blood_group,residence,password]
-    let result = await queryDB(query,[name,designation,department,id.toString(),dob,address,email,phone,blood_group,residence,password],true);
+    let result = await queryDB(query,[name,designation,department,id.toString(),dob,address,email,phone,blood_group,residence,password,user_id],true);
     // console.log(result);
     
     // res.status(200).json(result);
