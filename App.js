b2ac94873b8e4a4da20a4af465765a9bba778bbc
shamim2601 <shamim2601@gmail.com>
2022-02-10 22:33:33 +0600

    basic table routes done

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 const oracledb = require('oracledb');
 oracledb.outFormat = oracledb.OBJECT ;
 const express = require('express'); 
-const router = express.Router();
-const memtablerouter = require('./public/js/member_table');
-const revtablerouter = require('./public/js/reviews');
+const tablerouter = require('./public/js/basicTableRoutes');
 
 var app = new express(); 
 var port = 8080; 
 app.use(express.json());
-app.use(router);
 app.use(express.static('public'));   
 app.use(express.static('src/html'));
-app.use(memtablerouter);
-app.use(revtablerouter);
+app.use(tablerouter);
 
 app.listen(port, function(err) {  
     if (typeof(err) == "undefined") {  
---
+++ public/js/basicTableRoutes.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const oracledb = require('oracledb');
+oracledb.outFormat = oracledb.OBJECT ;
+const tablerouter = express.Router();
+
+let connection = undefined;
+async function dbQuery(query, params){
+  if(connection===undefined){
+    connection = await oracledb.getConnection({ user: "C##INSLIB", password: "PROJECT", connectionString: "localhost/orcl" });
+  }
+  try{
+    let result = await connection.execute(query,params);
+    return result.rows;
+  }catch(error){
+    console.log(error);
+  }
+}
+
+tablerouter.get("/members.html/bg", async (req,res)=>{
+  const query = "SELECT MEMBER_ID, MEMBER_NAME, TYPE, EMAIL, PHONE_NUMBER, BLOOD_GROUP FROM MEMBER ORDER BY BLOOD_GROUP";
+  const params = [];
+  const result = await dbQuery(query,params);
+  res.status(200).json(result);
+});
+
+tablerouter.get("/members.html/id", async (req,res)=>{
+  const query = "SELECT MEMBER_ID, MEMBER_NAME, TYPE, EMAIL, PHONE_NUMBER, BLOOD_GROUP FROM MEMBER ORDER BY MEMBER_ID";
+  const params = [];
+  const result = await dbQuery(query,params);
+  res.status(200).json(result);
+});
+
+tablerouter.get("/Reviews.html/list", async (req,res)=>{
+    const query = "SELECT B.COVER_IMAGE, B.BOOK_NAME,B.BOOK_ID, A.AUTHOR_NAME, M.MEMBER_NAME, RL.REVIEW_TEXT FROM  MEMBER M JOIN REVIEW_LIST RL ON (M.MEMBER_ID = RL.MEMBER_ID) JOIN BOOK B ON (RL.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (B.AUTHOR_ID = A.AUTHOR_ID) ORDER BY B.BOOK_NAME";
+    const params = [];
+    const result = await dbQuery(query,params);
+    res.status(200).json(result);
+});
+
+tablerouter.post('/searchRevByBookName', async (req,res)=>{
+    console.log(req.body)
+    res.redirect('/');
+});
+
+tablerouter.get("/links.html/fill", async (req,res)=>{
+    const query = "SELECT LINK_NAME,LINK_TEXT FROM LINKS ORDER BY LINK_NAME";
+    const params = [];
+    const result = await dbQuery(query,params);
+    res.status(200).json(result);
+});
+
+tablerouter.get("/Authors.html/fill", async (req,res)=>{
+    const query = "SELECT AUTHOR_NAME, NATIONALITY, LIFE_SPAN FROM AUTHOR ORDER BY AUTHOR_NAME";
+    const params = [];
+    const result = await dbQuery(query,params);
+    res.status(200).json(result);
+});
+
+tablerouter.get("/Publishers.html/fill", async (req,res)=>{
+    const query = "SELECT PUBLISHER_NAME, ADDRESS, PHONE_NUMBER FROM PUBLISHER";
+    const params = [];
+    const result = await dbQuery(query,params);
+    res.status(200).json(result);
+});
+
+module.exports = tablerouter;
