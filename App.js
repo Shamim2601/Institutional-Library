34bc6515308e330abcf7017444ccafa7f7e06ff3
shamim2601 <shamim2601@gmail.com>
2022-02-11 12:58:58 +0600

    ac books list done

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const oracledb = require('oracledb');
 oracledb.outFormat = oracledb.OBJECT ;
 const express = require('express'); 
 const tablerouter = require('./public/js/basicTableRoutes');
+const bookRouter = require('./public/js/bookRoutes');
 
 var app = new express(); 
 var port = 8080; 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.static('public'));   
 app.use(express.static('src/html'));
 app.use(tablerouter);
+app.use(bookRouter);
 
 app.listen(port, function(err) {  
     if (typeof(err) == "undefined") {  
---
+++ public/js/bookRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const oracledb = require('oracledb');
+const { subscribe } = require('./basicTableRoutes');
+oracledb.outFormat = oracledb.OBJECT ;
+const bookRouter = express.Router();
+
+let connection = undefined;
+async function dbQuery(query, params){
+  if(connection===undefined){
+    connection = await oracledb.getConnection({ user: "C##INSLIB", password: "PROJECT", connectionString: "localhost/orcl" });
+  }
+  try{
+    let result = await connection.execute(query,params);
+    return result.rows;
+  }catch(error){
+    console.log(error);
+  }
+}
+
+bookRouter.get('/:parent/:child/:grandchild', async (req,res)=>{
+    var info = req.params;
+    var query;
+    if(info.parent == 'ac_books.html'){
+       query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_ACADEMIC AC JOIN BOOK B ON (AC.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE AC.DEPARTMENT LIKE '${info.child}' AND AC.SUBJECT LIKE '${info.grandchild}' ORDER BY A.AUTHOR_NAME`;
+    }
+    const params = [];
+    const result = await dbQuery(query,params);
+    res.status(200).json(result);
+    
+});
+
+module.exports = bookRouter;
