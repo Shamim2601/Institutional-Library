46f1ca071607e6c2e3b4ae7305b57ba16ffcf28d
shamim2601 <shamim2601@gmail.com>
2022-02-12 16:22:20 +0600

    new arrivals done

--- public/js/basicTableRoutes.js
+++ public/js/basicTableRoutes.js
@@ -31,22 +31,17 @@ tablerouter.get("/members.html/id", async (req,res)=>{
 });
 
 tablerouter.get("/Reviews.html/academic", async (req,res)=>{
-    const query = "SELECT B.COVER_IMAGE, B.BOOK_NAME,B.BOOK_ID, A.AUTHOR_NAME, M.MEMBER_NAME, RL.REVIEW_TEXT FROM  MEMBER M JOIN REVIEW_LIST RL ON (M.MEMBER_ID = RL.MEMBER_ID) JOIN BOOK B ON (RL.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (B.AUTHOR_ID = A.AUTHOR_ID) WHERE B.BOOK_TYPE LIKE 'ACADEMIC' ORDER BY B.BOOK_NAME";
+    const query = "SELECT B.COVER_IMAGE, B.BOOK_NAME,B.BOOK_ID, A.AUTHOR_NAME, M.MEMBER_NAME, RL.REVIEW_TEXT FROM  MEMBER M JOIN REVIEW_LIST RL ON (M.MEMBER_ID = RL.MEMBER_ID) JOIN BOOK B ON (RL.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (B.AUTHOR_ID = A.AUTHOR_ID) WHERE UPPER(B.BOOK_TYPE) LIKE '%ACADEMIC%' ORDER BY B.BOOK_NAME";
     const params = [];
     const result = await dbQuery(query,params);
     res.status(200).json(result);
 });
 
 tablerouter.get("/Reviews.html/other", async (req,res)=>{
-  const query = "SELECT B.COVER_IMAGE, B.BOOK_NAME,B.BOOK_ID, A.AUTHOR_NAME, M.MEMBER_NAME, RL.REVIEW_TEXT FROM  MEMBER M JOIN REVIEW_LIST RL ON (M.MEMBER_ID = RL.MEMBER_ID) JOIN BOOK B ON (RL.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (B.AUTHOR_ID = A.AUTHOR_ID) WHERE B.BOOK_TYPE LIKE 'OTHER%' ORDER BY B.BOOK_NAME";
+  const query = "SELECT B.COVER_IMAGE, B.BOOK_NAME,B.BOOK_ID, A.AUTHOR_NAME, M.MEMBER_NAME, RL.REVIEW_TEXT FROM  MEMBER M JOIN REVIEW_LIST RL ON (M.MEMBER_ID = RL.MEMBER_ID) JOIN BOOK B ON (RL.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (B.AUTHOR_ID = A.AUTHOR_ID) WHERE UPPER(B.BOOK_TYPE) LIKE '%OTHER%' ORDER BY B.BOOK_NAME";
   const params = [];
   const result = await dbQuery(query,params);
   res.status(200).json(result);
-});
-
-tablerouter.post('/searchRevByBookName', async (req,res)=>{
-    console.log(req.body)
-    res.redirect('/');
 });
 
 tablerouter.get("/news_events.html/fill", async (req,res)=>{
@@ -71,7 +66,7 @@ tablerouter.get("/Authors.html/fill", async (req,res)=>{
 });
 
 tablerouter.get("/Publishers.html/fill", async (req,res)=>{
-    const query = "SELECT PUBLISHER_NAME, ADDRESS, PHONE_NUMBER FROM PUBLISHER";
+    const query = "SELECT PUBLISHER_NAME, ADDRESS, PHONE_NUMBER FROM PUBLISHER ORDER BY PUBLISHER_NAME";
     const params = [];
     const result = await dbQuery(query,params);
     res.status(200).json(result);
--- public/js/bookRoutes.js
+++ public/js/bookRoutes.js
@@ -21,15 +21,23 @@ bookRouter.get('/:parent/:child/:grandchild', async (req,res)=>{
     var info = req.params;
     var query;
     if(info.parent == 'ac_books.html'){
-       query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_ACADEMIC AC JOIN BOOK B ON (AC.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE AC.DEPARTMENT LIKE '${info.child}' AND AC.SUBJECT LIKE '${info.grandchild}' ORDER BY A.AUTHOR_NAME`;
+       query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_ACADEMIC AC JOIN BOOK B ON (AC.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE UPPER(AC.DEPARTMENT) LIKE '%${info.child}%' AND UPPER(AC.SUBJECT) LIKE '%${info.grandchild}%' ORDER BY A.AUTHOR_NAME`;
     }
     if(info.parent == 'other_books.html'){
-      query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_OTHERS OT JOIN BOOK B ON (OT.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE OT.CATEGORY LIKE '${info.child}' AND OT.GENRE LIKE '${info.grandchild}' ORDER BY A.AUTHOR_NAME`;
+      query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_OTHERS OT JOIN BOOK B ON (OT.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE UPPER(OT.CATEGORY) LIKE '%${info.child}%' AND UPPER(OT.GENRE) LIKE '%${info.grandchild}%' ORDER BY A.AUTHOR_NAME`;
    }
     const params = [];
     const result = await dbQuery(query,params);
     res.status(200).json(result);
     
 });
+
+bookRouter.get('/new_arrivals.html/fill', async (req,res)=>{
+  const query = "SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.BOOK_TYPE, B.LANGUAGE, B.YEAR, B.EDITION, B.COVER_IMAGE"+
+                " FROM BOOK B JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE ((SYSDATE-B.DATE_OF_ARRIVAL)/30)<=1 ORDER BY A.AUTHOR_NAME";
+  const params = [];
+  const result = await dbQuery(query,params);
+  res.status(200).json(result);
+})
 
 module.exports = bookRouter;
