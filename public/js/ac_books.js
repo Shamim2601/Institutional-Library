110404f0ab496b25ead05993f7208f2f1b944d1a
shamim2601 <shamim2601@gmail.com>
2022-02-21 00:13:28 +0600

    done by adding extra button

--- public/js/ac_books.js
+++ public/js/ac_books.js
@@ -11,14 +11,13 @@ router.get('/',(req,res)=>{
     res.render('ac_books.ejs');
 })
 router.get('/searchTable',async (req,res)=>{
-    console.log('LIKE FOR LIKE')
     let query = `SELECT BOOK.BOOK_ID, BOOK.BOOK_NAME, AUTHOR.AUTHOR_NAME, BOOK.PUBLISHER_NAME, BOOK.STATUS, BOOK.LANGUAGE, BOOK.YEAR, BOOK.EDITION, BOOK.NO_OF_PAGES,COVER_IMAGE
     FROM BOOK JOIN AUTHOR ON (BOOK.AUTHOR_ID = AUTHOR.AUTHOR_ID)
-    WHERE AUTHOR.AUTHOR_ID = 'AH'`
+    WHERE (BOOK.BOOK_NAME IS NOT NULL AND BOOK.BOOK_NAME LIKE :1) OR (AUTHOR.AUTHOR_NAME IS NOT NULL AND AUTHOR.AUTHOR_NAME LIKE :2)` //need to modify this query
     let book = '%' + req.session.bookName + '%'
     let author = '%' + req.session.author + '%';
-    let params = [book,author]
-    let result = await queryDB(query,[],false);
+    let params = [book.toUpperCase(),author.toUpperCase()]
+    let result = await queryDB(query,params,false);
     res.status(200).json(result.rows);
 })
 router.post('/', async function (req,res){
