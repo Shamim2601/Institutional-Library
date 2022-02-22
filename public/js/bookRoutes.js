040b0db5a2e1f93b815c9626ae1135380bcfa008
shamim2601 <shamim2601@gmail.com>
2022-02-22 22:00:06 +0600

    language in ac and other book

--- public/js/bookRoutes.js
+++ public/js/bookRoutes.js
@@ -14,17 +14,17 @@ async function dbQuery(query, params){
     let result = await connection.execute(query,params);
     return result.rows;
   }catch(error){
-    console.log(error);
+    console.log('error');
   }
 }
 
 bookRouter.get('/:parent/:child/:grandchild', async (req,res)=>{
     var info = req.params;
     let query;
-    if(info.parent == 'ac_books.html'){
+    if(info.parent == 'ac_books'){
        query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_ACADEMIC AC JOIN BOOK B ON (AC.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE UPPER(AC.DEPARTMENT) LIKE '%${info.child}%' AND UPPER(AC.SUBJECT) LIKE '%${info.grandchild}%' ORDER BY A.AUTHOR_NAME`;
     }
-    if(info.parent == 'other_books.html'){
+    if(info.parent == 'other_books'){
       query = `SELECT B.BOOK_ID, B.BOOK_NAME, A.AUTHOR_NAME, B.PUBLISHER_NAME, B.STATUS, B.LANGUAGE, B.YEAR, B.EDITION, B.NO_OF_PAGES, B.COVER_IMAGE FROM BOOKLIST_OTHERS OT JOIN BOOK B ON (OT.BOOK_ID = B.BOOK_ID) JOIN AUTHOR A ON (A.AUTHOR_ID = B.AUTHOR_ID) WHERE UPPER(OT.CATEGORY) LIKE '%${info.child}%' AND UPPER(OT.GENRE) LIKE '%${info.grandchild}%' ORDER BY A.AUTHOR_NAME`;
    }
     const params = [];
